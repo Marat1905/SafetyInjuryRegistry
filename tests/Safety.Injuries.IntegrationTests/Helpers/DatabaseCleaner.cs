461136bb89d3ccd74172ab04f4a6abb625c247e0
Marat <milya010790@mail.ru>
2026-06-08 14:40:35 +0500

    Добавление интеграциооных тестов

---
+++ tests/Safety.Injuries.IntegrationTests/Helpers/DatabaseCleaner.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Safety.Injuries.Infrastructure.Data;
+
+namespace Safety.Injuries.IntegrationTests.Helpers;
+public static class DatabaseCleaner
+{
+    public static async Task CleanDatabaseAsync(SafetyInjuriesDbContext context)
+    {
+        
+
+        foreach (var entry in context.ChangeTracker.Entries())
+        {
+            entry.State = EntityState.Detached;
+        }
+    }
+}
+
